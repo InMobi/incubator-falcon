71127cc54d10efb9998eb69bac8a1607b7acc16f
Shwetha GS <shwetha.gs@inmobi.com>
2014-10-15 17:36:53 +0530

    fixed test that fails on jenkins

--- common/src/test/java/org/apache/falcon/update/UpdateHelperTest.java
+++ common/src/test/java/org/apache/falcon/update/UpdateHelperTest.java
@@ -81,6 +81,9 @@ public class UpdateHelperTest extends AbstractTestBase {
         storeEntity(EntityType.FEED, "impressionFeed");
         storeEntity(EntityType.FEED, "imp-click-join1");
         storeEntity(EntityType.FEED, "imp-click-join2");
+
+        FileSystem fs = dfsCluster.getFileSystem();
+        fs.delete(new Path("/projects/falcon/clicks/"));
     }
 
     private void prepare(Process process) throws IOException, FalconException {
