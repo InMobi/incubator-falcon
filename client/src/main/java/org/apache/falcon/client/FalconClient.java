2db513ed776267cbdde7966695a2ea9b5aafeb35
Suhas V <suhas.v@inmobi.com>
2014-10-20 18:23:14 +0530

    Expose job id for running jobs in Falcon

--- client/src/main/java/org/apache/falcon/client/FalconClient.java
+++ client/src/main/java/org/apache/falcon/client/FalconClient.java
@@ -762,6 +762,13 @@ public class FalconClient {
                     }
                     sb.append("\n");
                 }
+                if (instance.actions != null) {
+                    sb.append("actions:\n");
+                    for (InstancesResult.InstanceAction action : instance.actions) {
+                        sb.append("    ").append(action.getAction()).append("\t");
+                        sb.append(action.getStatus()).append("\t").append(action.getLogFile()).append("\n");
+                    }
+                }
 
             }
         }
